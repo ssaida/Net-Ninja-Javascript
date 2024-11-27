fc50c29f55f9eb9b80f35b9709769d8c611258a5
Samer Saida <ssaida@gmail.com>
2024-11-27 11:54:31 +0200

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,19 @@
+var num = 6;
+console.log(num);
+ let age = 43;
+ let year = 2024;
+
+ console.log(age);
+ console.log(year);
+
+ const minutes = 60;
+ console.log(minutes);
+//  minutes = 33;
+ console.log(minutes); 
+
+ var score = 75;
+ console.log(score);
+ score += 1;
+ console.log(score);
+
+ 
